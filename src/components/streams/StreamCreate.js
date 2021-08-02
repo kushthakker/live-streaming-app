f8b802f1022501b6354eec5ac72379436acaf8f3
kushthakker <kushthakker777@gmail.com>
2021-08-02 15:36:32 +0530

    working

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -2,17 +2,38 @@ import React from "react";
 import HomeForm from "../HomeForm";
 import { connect } from "react-redux";
 import { CreateStream } from "../../actions";
+import { Redirect } from "react-router-dom";
 
-const StreamCreate = ({ CreateStream }) => {
+const StreamCreate = ({ CreateStream, auth, stream }) => {
   const edit = (data) => {
     CreateStream(data);
   };
+
+  const render = () => {
+    return (
+      <div>
+        <h2>Create a Stream</h2>
+        <HomeForm onSubmit={edit} />
+      </div>
+    );
+  };
+
   return (
-    <div>
-      <h2>Create a Stream</h2>
-      <HomeForm onSubmit={edit} />
-    </div>
+    <>
+      {auth.isSignedIn === null ? (
+        <div>Loading...</div>
+      ) : auth.isSignedIn === true ? (
+        render()
+      ) : (
+        <Redirect to="/" />
+      )}
+    </>
   );
 };
-
-export default connect(null, { CreateStream })(StreamCreate);
+const mapStateToProps = (state, ownProps) => {
+  return {
+    auth: state.auth,
+    stream: state.streams[ownProps.match.params.id],
+  };
+};
+export default connect(mapStateToProps, { CreateStream })(StreamCreate);
